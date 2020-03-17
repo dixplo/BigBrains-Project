8c78f8250e0615b3f6f4ebd416e59cc336f74f16
nemoo0 <damien.royer140@gmail.com>
2020-03-17 11:04:43 +0100

    adding routes

--- app/models/project.js
+++ app/models/project.js
@@ -3,9 +3,8 @@ const { Model } = DS;
 
 export default Model.extend({
   name:DS.attr('string'),
-  decriptif:DS.attr('string'), // proprieté persistente
+  descriptif:DS.attr('string'), // proprieté persistente
   startDate:DS.attr('date'),
   dueDate:DS.attr('date')
-
 
 });
