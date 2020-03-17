e685ef1085f395d270e5f3fa865f01ae183f2828
nemoo0 <damien.royer140@gmail.com>
2020-03-17 10:50:14 +0100

    adding attributes for models

--- app/models/project.js
+++ app/models/project.js
@@ -2,5 +2,10 @@ import DS from 'ember-data';
 const { Model } = DS;
 
 export default Model.extend({
+  name:DS.attr('string'),
+  decriptif:DS.attr('string'), // proprieté persistente
+  startDate:DS.attr('date'),
+  dueDate:DS.attr('date')
+
 
 });
