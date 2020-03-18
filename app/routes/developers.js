e8afc73058050cbdebcd68af9456f2b05f3c7b10
nemoo0 <damien.royer140@gmail.com>
2020-03-18 11:11:19 +0100

    remove developers

--- app/routes/developers.js
+++ app/routes/developers.js
@@ -3,15 +3,21 @@ import Route from '@ember/routing/route';
 import Developers from 'boards/class/developers-class';
 
 export default Route.extend({
+  model() {
+          return this.get('store').findAll('developer');
+      },
   actions:{
-    model() {
-            return this.get('store').findAll('developer');
-        },
-    addNew(identity, name){
-      let store=this.get('store');
-      let developer = this.get('store').createRecord('developer',{identity: identity, name: name}); // developer désigne le model
+    addNew(identity, name, fname){
+      if (!identity || !name || !fname) {
+        console.log("bien");
+        let store=this.get('store');
+        let developer = this.get('store').createRecord('developer',{identity: identity, name: name, fname: fname}); // developer désigne le model
+        developer.save();
+      }
+    },
+    remove(developer){
+      developer.deleteRecord();
       developer.save();
     }
-
   }
 });
