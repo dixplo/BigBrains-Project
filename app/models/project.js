94f7fbc9e1794432c3c7c628db39bc9e4e86d5e6
Léo Texier <55082513+LeoTexier@users.noreply.github.com>
2020-03-19 09:23:10 +0100

    testAddUtc

--- app/models/project.js
+++ app/models/project.js
@@ -2,9 +2,12 @@ import DS from 'ember-data';
 const { Model } = DS;
 
 export default Model.extend({
-  name:DS.attr('string'),
-  descriptif:DS.attr('string'), // proprieté persistente
-  startDate:DS.attr('date'),
-  dueDate:DS.attr('date')
+  name: DS.attr('string'),
+  descriptif: DS.attr('string'), // proprietÃ© persistente
+  startDate: DS.attr('utc'),
+  dueDate: DS.attr('utc')
+
+
+
 
 });
