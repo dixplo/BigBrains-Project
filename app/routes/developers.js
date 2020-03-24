c4b90228051f819f5cd2d3a75808a30fe19be4a1
Léo Texier <55082513+LeoTexier@users.noreply.github.com>
2020-03-24 11:53:18 +0100

    addremoveDev and projectDeleteModal

--- app/routes/developers.js
+++ app/routes/developers.js
@@ -1,26 +1,30 @@
 // Route developpers
 import Route from '@ember/routing/route';
-import Developers from 'boards/class/developers-class';
+import RSVP from 'rsvp';
+import EmberObject from '@ember/object';
+import { set } from '@ember/object';
 
 export default Route.extend({
   model() {
-          return this.get('store').findAll('developer');
+    return RSVP.hash({
+      developers: this.get('store').findAll('developer'),
+      dev: EmberObject.create()
+        });
       },
   actions:{
-    addNew(m,identity, name, fname){
-      if(!identity || !name || !fname){
-
-      }
+    addNew(dev,ident,name,fname){
+      if(!ident || !name || !fname){}
       else{
-        let store=this.get('store');
-        m.set('content',"");
-        let developer = this.get('store').createRecord('developer',{identity: identity, name: name,fname:fname}); // developer désigne le model
-        developer.save();
-      }
-    },
+        this.get('store').createRecord('developer',dev).save();
+      }},
     remove(developer){
       developer.deleteRecord();
       developer.save();
+    },
+    //A finir => validation de suppression d'un dev
+    confirmDelete(model,dev){
+      model.set('toDelete',dev);
+      $('.ui.modal.delete').modal('setting', 'closable', true).modal('show');
     }
-  }
+}
 });
--- app/routes/projects.js
+++ app/routes/projects.js
@@ -1,4 +1,5 @@
 import Route from '@ember/routing/route';
+import { set } from '@ember/object';
 
 export default Route.extend({
   model() {
@@ -19,6 +20,10 @@ export default Route.extend({
     remove(project){
       project.deleteRecord();
       project.save();
-    }
+    },
+    confirmDelete(model,project){
+      model.set('toDelete',project);
+      $('.ui.modal.delete').modal('setting', 'closable', true).modal('show');
+}
   }
 });
