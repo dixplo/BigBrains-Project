c2dcd6e0f27e0daeddbed2f2fd9164830ed5cfa1
nemoo0 <damien.royer140@gmail.com>
2020-03-26 21:01:15 +0100

    Improving add project

--- app/routes/projects/new.js
+++ app/routes/projects/new.js
@@ -1,4 +1,23 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
+import EmberObject from '@ember/object';
+import { set,get } from '@ember/object';
 
 export default Route.extend({
+  actions:{
+      addNew(model){
+
+        this.get('store').createRecord('project',{
+          name: get(model,"name"),
+          descriptif: get(model,"descriptif"),
+          startDate: get(model,"startDate"),
+          dueDate: get(model,"dueDate"),
+        }).save();
+        this.transitionTo("/projects");
+
+      },
+      backToProj(){
+        this.transitionTo("/projects");
+      }
+  }
 });
