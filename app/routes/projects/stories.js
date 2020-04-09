8548843c83e042d2d81982119f7f03031b88957e
Léo Texier <55082513+LeoTexier@users.noreply.github.com>
2020-04-09 09:05:08 +0200

    addStories/resolveDisplayStories

--- app/routes/projects/stories.js
+++ app/routes/projects/stories.js
@@ -8,31 +8,4 @@ export default Route.extend({
   model(params){
     return this.get('store').findRecord('project',params.project_id);
   }
-
-
-
-  /*async model(params){
-    let proj = await this.get('store').findRecord('project',params.project_id); // definition "proj"
-    return RSVP.hash({
-      proj: proj,
-      name: get(proj, "name"),
-      descriptif: get(proj, "descriptif"),
-      startDate: get(proj, "startDate"),
-      dueDate: get(proj, "dueDate")
-    })*/
-/*  },
-  async modal(params){
-    let story = await this.store.query('story',{
-      filter: {project: param.project_id}});
-    return RSVP.hash({
-      story: story,
-      code: get(story,'code'),
-      descriptif: get(story,'descriptif')
-    })
-  }*/
-  /*renderTemplate() {
-        this.render('projects.stories','projects', {
-            into: 'projects.stories'
-        });
-    }*/
 });
--- app/routes/projects/stories/new.js
+++ app/routes/projects/stories/new.js
@@ -1,4 +1,17 @@
 import Route from '@ember/routing/route';
+import { set,get } from '@ember/object';
 
 export default Route.extend({
+  actions:{
+   addNew(code,description, project){
+     let story = this.get('store').createRecord('story',{code:code,description:description});
+     set(story,'project',project);
+     story.save().then(()=>{
+       project.save().then(()=>{this.transitionTo('projects');});
+     });
+   },
+   cancel() {
+     this.transitionTo('projects');
+   }
+ }
 });
