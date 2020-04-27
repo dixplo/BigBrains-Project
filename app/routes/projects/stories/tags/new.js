f2c0e6429602b145fb0532d178df201823ed52b5
Léo Texier <55082513+LeoTexier@users.noreply.github.com>
2020-04-27 09:28:03 +0200

    addTagsDev

--- app/routes/projects/stories/tags/new.js
+++ app/routes/projects/stories/tags/new.js
@@ -2,8 +2,25 @@ import Route from '@ember/routing/route';
 import { set,get } from '@ember/object';
 
 export default Route.extend({
+  colors: [{ name: "red", hexa: "#DB2828" },
+            { name: "orange", hexa: "#F2711C" },
+            { name: "yellow", hexa: "#FBBD08" },
+            { name: "olive", hexa: "#b5cc18" },
+            { name: "green", hexa: "#24BA45" },
+            { name: "teal", hexa: "#00B5AD" },
+            { name: "blue", hexa: "#2185D0" },
+            { name: "purple", hexa: "#A333C8" },
+            { name: "pink", hexa: "#E03997" },
+            { name: "brown", hexa: "#A5673F" },
+            { name: "white", hexa: "#E5E5E5" },
+            { name: "grey", hexa: "#767676" },
+            { name: "black", hexa: "#1B1C1D" }],
   actions:{
- addNew(title,color,id,story){
+
+  setColor(color,model){
+      set('model','color',color);
+    },
+  addNew(title,color,id,story){
    let error = !title
    if(error){
      set(story,'error',error);
