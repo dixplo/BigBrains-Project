f6514b105e4f5c2e43b97fba53e395ee7c6056f9
Léo Texier <55082513+LeoTexier@users.noreply.github.com>
2020-05-06 13:29:02 +0200

    addDesign

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
+import EmberObject from '@ember/object';
+import RSVP from 'rsvp';
 
 export default Route.extend({
+  async model() {
+    EmberObject.create(this.store.findAll('project', { reload: true }));
+    return RSVP.hash({
+      projects: this.store.findAll('project', { reload: true })
+    });
+  }
 });
